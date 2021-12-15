1cea29072ecb9452c931935d72d9c4397c7fda37
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-15 01:00:23 +0000

    Refactor character counter js for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,15 @@
 $(document).ready(function() {
 
 
-  const input = $('#tweet-text');
-  input.on('input', function(event) {
-    const counter = $('.counter');
-    if (input.val().length > 140) {
+  // On input, update counter value & highlight if over 140 chars
+  const textarea = $('#tweet-text');
+  const counter = $('.counter');
+  textarea.on('input', function(event) {
+    counter.text(140 - textarea.val().length);
+    if (textarea.val().length > 140) {
       counter.toggleClass('overlimit', true);
     } else {
       counter.toggleClass('overlimit', false);
     }
-    counter.text(140 - input.val().length);
   });
 });
