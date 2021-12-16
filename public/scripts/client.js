3ab052ea0fc19e09583ec4b86e7c7f6c673d3d20
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-16 10:59:25 +0000

    Add CSS to allow word-breaks in order to prevent overflow of the tweet body

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,44 +35,6 @@ const createTweetElement = function(tweetObj) {
   return $twArticle;
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Kurtis",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@kurtisbgrant"
-    },
-    "content": {
-      "text": "If a man knows not which port he sails, no wind is favorable. —Marcus Aurelius —Kurtis Grant"
-    },
-    "created_at": 1637485006795
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(() => {
 
   // Handle new tweet form submission POST req
