498ac8d775f122027a2739eb8d8207b7f4798c7f
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-16 11:25:32 +0000

    Update composer-char-counter to update on document loaded & to use "$" variable names

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,20 @@
 $(document).ready(function() {
 
 
-  // On input, update counter value & highlight if over 140 chars
-  const textarea = $('#tweet-text');
-  const counter = $('.counter');
-  textarea.on('input', function(event) {
-    counter.text(140 - textarea.val().length);
-    if (textarea.val().length > 140) {
-      counter.toggleClass('overlimit', true);
+  // On input (and on load), update counter
+  // value and highlight if over 140 chars
+  const $textarea = $('#tweet-text');
+  const $counter = $('.counter');
+  const updateCharCountAndOverlimitClass = function($textarea, $counter) {
+    $counter.text(140 - $textarea.val().length);
+    if ($textarea.val().length > 140) {
+      $counter.toggleClass('overlimit', true);
     } else {
-      counter.toggleClass('overlimit', false);
+      $counter.toggleClass('overlimit', false);
     }
+  };
+  updateCharCountAndOverlimitClass($textarea, $counter);
+  textarea.on('input', function(event) {
+    updateCharCountAndOverlimitClass($textarea, $counter);
   });
 });
